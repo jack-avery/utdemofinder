faa922fae0bc50eb79042593c0f8fe895bac46fa
jack-avery <email.jack.avery@gmail.com>
2023-03-13 10:26:39 -0400

    add demo folder saving for future feature

--- main.py
+++ main.py
@@ -5,6 +5,8 @@
 #
 #   Jack Avery 2023
 ###
+import json
+import os
 import requests
 import re
 import tkinter as tk
@@ -12,6 +14,9 @@ version = '1.0.2'
 
 STEAMID_RE = re.compile(r"\d+")
 """Regex to compare Steam UserID64s to to validate"""
+
+CONFIG_PATH = "config.txt"
+"""Path to the user config file to use"""
 
 class Application:
     def __init__(self):
@@ -21,30 +26,43 @@ class Application:
         self.root.configure(bg="#444444")
         self.root.resizable(0, 0)
 
-        tk.Label(master=self.root, text="Steam ID64:", bg="#444444", fg="white")\
+        demo_folder = "C:/Program Files (x86)/Steam/steamapps/common/Team Fortress 2/tf"
+        if os.path.exists(CONFIG_PATH):
+            with open(CONFIG_PATH, "r") as cfgfile:
+                cfg = json.loads(cfgfile.read())
+            demo_folder = cfg["demo_folder"]
+
+        tk.Label(master=self.root, text="TF2 Folder:", bg="#444444", fg="white")\
             .place(x=0, y=0, width=64, height=24)
+        self.folder_input = tk.Text(
+            master=self.root, bg="#444444", fg="white", height=1, width=8, wrap="none")
+        self.folder_input.place(x=70, y=0, width=410, height=24)
+        self.folder_input.insert(tk.END, demo_folder)
+
+        tk.Label(master=self.root, text="Steam ID64:", bg="#444444", fg="white")\
+            .place(x=0, y=24, width=64, height=24)
         self.search_id64 = tk.Text(
-            master=self.root, bg="#444444", fg="white", height=1, width=8)
-        self.search_id64.place(x=70, y=0, width=410, height=24)
+            master=self.root, bg="#444444", fg="white", height=1, width=8, wrap="none")
+        self.search_id64.place(x=70, y=24, width=410, height=24)
 
         tk.Label(master=self.root, text="Map:", bg="#444444", fg="white")\
-            .place(x=0, y=24, width=64, height=24)
+            .place(x=0, y=48, width=64, height=24)
         self.search_map = tk.Text(
-            master=self.root, bg="#444444", fg="white", height=1, width=8)
-        self.search_map.place(x=70, y=24, width=410, height=24)
+            master=self.root, bg="#444444", fg="white", height=1, width=8, wrap="none")
+        self.search_map.place(x=70, y=48, width=410, height=24)
 
         tk.Label(master=self.root, text="Played With:", bg="#444444", fg="white")\
-            .place(x=0, y=48, width=64, height=24)
+            .place(x=0, y=72, width=64, height=24)
         self.search_with = tk.Text(
-            master=self.root, bg="#444444", fg="white", height=1, width=8)
-        self.search_with.place(x=70, y=48, width=410, height=24)
-
-        self.infobox = tk.Listbox(self.root, bg="#444444", fg="white")
-        self.infobox.place(x=0, y=108, width=480, height=172)
+            master=self.root, bg="#444444", fg="white", height=1, width=8, wrap="none")
+        self.search_with.place(x=70, y=72, width=410, height=24)
 
         self.go = tk.Button(
             text="Go", command=self.get_demos, bg="#004400", fg="white")
-        self.go.place(x=0, y=72, width=480, height=36)
+        self.go.place(x=0, y=96, width=480, height=36)
+
+        self.infobox = tk.Listbox(self.root, bg="#444444", fg="white")
+        self.infobox.place(x=0, y=132, width=480, height=148)
 
         self.log("Enter a Steam ID64 and press Go.")
         self.log("A new window will open up with the results.")
@@ -58,9 +76,17 @@ class Application:
         self.log("")
         self.log("Searching...")
 
+        demofolder = self.folder_input.get("1.0", "end-1c")
         id = self.search_id64.get("1.0", "end-1c")
         map = self.search_map.get("1.0", "end-1c")
         id_with = self.search_with.get("1.0", "end-1c")
+
+        with open(CONFIG_PATH, "w") as cfgfile:
+            data = {
+                "demo_folder": demofolder
+            }
+
+            cfgfile.writelines(json.dumps(data, indent=4))
 
         if not STEAMID_RE.match(id):
             self.log("Invalid ID64 for \"Steam ID\".")
